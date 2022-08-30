7a9cda3fa3dac730af0bbba36e5d9189c6cac6b6
Lucas Mateus <lmateus1067@outlook.com>
2022-08-30 17:44:16 -0300

    recebaaaa

--- Testes e aprendizagem/usandosprites2.py
+++ Testes e aprendizagem/usandosprites2.py
@@ -26,7 +26,6 @@ class Gato(pygame.sprite.Sprite):
         self.sprites.append(pygame.image.load("sprites/sprite_gatinho3.png"))
         self.sprites.append(pygame.image.load("sprites/sprite_gatinho4.png"))
         self.sprites.append(pygame.image.load("sprites/sprite_gatinho5.png"))
-
         self.sprites.append(pygame.image.load("sprites/gatinho_esquerda0.png"))
         self.sprites.append(pygame.image.load("sprites/gatinho_esquerda1.png"))
         self.sprites.append(pygame.image.load("sprites/gatinho_esquerda2.png"))
@@ -37,45 +36,42 @@ class Gato(pygame.sprite.Sprite):
         self.atual = 0
         self.image = self.sprites[self.atual]
         # aumentando a escala das sprites
-
-        self.sprites2 = []
         self.image = pygame.transform.scale(self.image, (72*2, 60*2))
 
         # transformando num quadrado e dizendo onde quero a imagem
         self.rect = self.image.get_rect()
         self.rect.topleft = 300, 330
 
-        self.animar = False
         self.andadireita = False
         self.andaesquerda = False
 
-        
-
-
-
     def update(self):
-        if self.animar == True:
-            if self.andaesquerda == True:
-                self.atual += 0.15
-                if self.atual >= (len(self.sprites)//2):
-                    self.atual = 0
-                    self.animar = False
+        
+        if self.andaesquerda == True:
+            # self.atual = 0
+            self.atual += 0.15
+            if self.atual >= (len(self.sprites)/2):
+                self.atual = 0
+                self.andaesquerda = False
 
-            elif self.andadireita == True:
-                self.atual = ((len(self.sprites))//2 ) + 1
-                self.atual += 0.15
-                if self.atual >= len(self.sprites):
-                    self.atual = ((len(self.sprites))//2 ) + 1
-                    self.animar = False
+        elif self.andadireita == True:
+            if self.atual < 6: #RECEBAAAAA ESSA GAMBIARRA NO SEU PEITÃO!!!!
+                self.atual += 6 - self.atual
+            self.atual += 0.15
+            if self.atual >= len(self.sprites):
+                self.atual = 6
+                self.andadireita = False
+
         self.image = self.sprites[int(self.atual)]
         self.image = pygame.transform.scale(self.image, (72*2, 60*2))
 
-    def andar(self, direção):
-        if direção == 0:
-            self.andadireita = True
-        elif direção == 1:
-            self.andaesquerda = True
-        self.animar = True
+    def andardireita(self):
+        self.andadireita = True
+        self.andaesquerda = False
+
+    def andaresquerda(self):
+        self.andaesquerda = True
+        self.andadireita = False
 
 
 todas_as_sprites = pygame.sprite.Group()
@@ -83,7 +79,9 @@ gato = Gato()
 todas_as_sprites.add(gato)
 
 imagem_fundo = pygame.image.load("sprites/cidade_fundo.jpg").convert()
-imagem_fundo = pygame.transform.scale(imagem_fundo, (largura_janela, altura_janela)) #convertendo a escala pra se adequar a minha tela
+# convertendo a escala pra se adequar a minha tela
+imagem_fundo = pygame.transform.scale(
+    imagem_fundo, (largura_janela, altura_janela))
 
 
 while True:
@@ -97,14 +95,12 @@ while True:
             pygame.quit()
             exit()
 
-        if event.type == KEYDOWN:
-            if event.key == K_d:
-                gato.andar(0)
-            elif event.key == K_a:
-                gato.andar(1)
+        if pygame.key.get_pressed()[K_d]:
+            gato.andardireita()
+        if pygame.key.get_pressed()[K_a]:
+            gato.andaresquerda()
 
-
-    tela_jogo.blit(imagem_fundo, (0,0))
+    tela_jogo.blit(imagem_fundo, (0, 0))
     todas_as_sprites.draw(tela_jogo)
     todas_as_sprites.update()
     pygame.display.flip()
